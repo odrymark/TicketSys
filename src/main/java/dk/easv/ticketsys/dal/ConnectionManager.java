b18d489b2642bae1a7e56f5c41986f2f64eb23a3
Tamás Major <majortamas313@gmail.com>
2025-03-18 13:57:24 +0100

    DB changed

--- src/main/java/dk/easv/ticketsys/dal/ConnectionManager.java
+++ src/main/java/dk/easv/ticketsys/dal/ConnectionManager.java
@@ -9,17 +9,19 @@ import java.sql.Connection;
 public class ConnectionManager {
     private final SQLServerDataSource ds;
 
-    public ConnectionManager() throws TicketExceptions {
+    public ConnectionManager() throws TicketExceptions
+    {
         ds = new SQLServerDataSource();
         ds.setServerName("EASV-DB4");
-        ds.setDatabaseName("ervbac01_ticket");
+        ds.setDatabaseName("EASV_Ticket_moet");
         ds.setPortNumber(1433);
-        ds.setUser("CSe2024b_e_8");
-        ds.setPassword("CSe2024bE8!24");
+        ds.setUser("CSe2024b_e_28");
+        ds.setPassword("CSe2024bE28!24");
         ds.setTrustServerCertificate(true);
     }
 
-    public Connection getConnection() throws SQLServerException {
+    public Connection getConnection() throws SQLServerException
+    {
         return ds.getConnection();
     }
 }
