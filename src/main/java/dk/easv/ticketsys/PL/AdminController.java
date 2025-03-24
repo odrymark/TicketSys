b9f08117e88605f88ee57cf93e4ff584c5d68f83
odrym <odrymark@gmail.com>
2025-03-24 11:12:21 +0100

    Window scaling + UI fixes

--- src/main/java/dk/easv/ticketsys/PL/AdminController.java
+++ src/main/java/dk/easv/ticketsys/PL/AdminController.java
@@ -10,6 +10,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -41,6 +42,15 @@ public class AdminController
     private ObservableList<HBox> events = FXCollections.observableArrayList();
     private ObservableList<VBox> users = FXCollections.observableArrayList();
     private final Button temp = new Button();
+    @FXML
+    private ScrollPane scrollP;
+
+    @FXML
+    public void initialize()
+    {
+        eventsPane.prefWidthProperty().bind(scrollP.widthProperty());
+        eventsPane.prefHeightProperty().bind(scrollP.heightProperty());
+    }
 
     @FXML
     private void eventsTab()
